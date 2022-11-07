64c12c4ba1e01fd811ea116c0000674c3a27fa55
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2022-11-07 21:47:50 +0300

    first commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,16 @@
+import './Main.scss';
+import { data } from '../../helper/data';
+import Card from './Card';
+
+const Main = () => {
+    
+    return(
+        <div className="card-container">
+            {data.map((item)=> <Card {...item} />
+            )}
+        </div>
+    )
+}
+
+
+export default Main;
